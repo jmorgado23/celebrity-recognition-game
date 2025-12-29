3d101575b33d58e4121094f2fb15767a06a61ad4
jmorgado23 <80257941+jmorgado23@users.noreply.github.com>
2025-12-29 23:29:33 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,17 @@
+window.CONFIG = {
+  siteName: "Celebrity Recognition Game",
+
+  title: "Celebrity Recognition Game",
+  description: "Test how well you recognize famous people.",
+
+  datasetUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vS52keBl1q6F8AVumY7iwghhuYyxm5n9XJdyHFVlBIdWTp4hN-IPvwseIo5cjJO_iUzJp7H6EQMIT6E/pub?output=csv",
+
+  totalPerRound: 20,
+  pointsPerCorrect: 10,
+
+  itemLabel: "celebrity",
+
+  shareTemplate: "I scored {score} / {max} on Celebrity Recognition Game. Can you beat me?",
+
+  themeColor: "#5a67ff"
+};
